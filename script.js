afccdaf295473539434f1465908af233dad7ed2a
caina <cainazuma@yahoo.com>
2023-11-06 21:34:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+function verificar() {
+  var data = new Date();
+  var ano = data.getFullYear();
+  var fano = document.getElementById("txtano");
+  var res = document.querySelector("div#res");
+
+  if (fano.value.length == 0 || Number(fano.value) > ano) {
+    window.alert("Verifique os dados e tente novamente!");
+  } else {
+    var fsex = document.getElementsByName("radsex");
+    var idade = ano - Number(fano.value);
+    var gênero = "";
+    var img = document.createElement("img");
+    img.setAttribute("id", "foto");
+
+    if (fsex[0].checked) {
+      gênero = "Homem";
+      if (idade >= 0 && idade <= 12) {
+        // Criança
+        img.setAttribute("src", "imagens/bebeH.jpg");
+      } else if (idade < 21) {
+        // Jovem
+        img.setAttribute("src", "imagens/jovemH.jpg");
+      } else if (idade < 50) {
+        // Adulto
+        img.setAttribute("src", "imagens/adultoH.jpg");
+      } else {
+        // Idoso
+        img.setAttribute("src", "imagens/idosoH.jpg");
+      }
+    } else if (fsex[1].checked) {
+      gênero = "Mulher";
+      if (idade >= 0 && idade <= 12) {
+        // Criança
+        img.setAttribute("src", "imagens/bebeF.jpg");
+      } else if (idade < 21) {
+        // Jovem
+        img.setAttribute("src", "imagens/jovemF.jpg");
+      } else if (idade < 50) {
+        // Adulta
+        img.setAttribute("src", "imagens/adultaF.jpg");
+      } else {
+        // Idosa
+        img.setAttribute("src", "imagens/idosaF.jpg");
+      }
+    }
+    res.style.textAlign = "center";
+    res.innerHTML = `Dectectamos: ${gênero} com ${idade} anos`;
+    res.appendChild(img);
+  }
+}
